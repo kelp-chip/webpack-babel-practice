19403c3feaabc178427be3bce021954118b0b404
Lexy Kramer <lexy.kramer@gmail.com>
2021-09-20 22:02:00 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,10 +6,6 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, '..')));
 
-app.use('/', (req, res) => {
-  res.render('index.html')
-})
-
 app.listen(port, () => {
   console.log(`Server listening at localhost:${port}!`);
 });
